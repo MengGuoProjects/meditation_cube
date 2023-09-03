5096e876af61461628369653adee6d2b7ae7994a
Meng Guo <Meng.guo.HH@gmail.com>
2023-09-03 21:02:58 +0200

    feat: implement simple breath in function

--- meditation_cube.ino
+++ meditation_cube.ino
@@ -1,12 +1,34 @@
 // Meditation cube firmware 
 // by Meng Guo
 
-void setup() {
-  // put your setup code here, to run once:
+/* global variables */
+const unsigned int led_1_pin = 11;
 
+/* initializtion */
+void setup() {
+  pinMode(led_1_pin, OUTPUT); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/* forward clarations */
+void breath_in(const unsigned int duration_to_breathin_s);
+int conv_percent_to_ADC(int input);
 
+/* main function */
+void loop() {
+  const unsigned int time_breath_in_s = 5;
+  breath_in(time_breath_in_s);
+}
+
+/* helper functions */
+void breath_in(const unsigned int duration_to_breathin_s) {
+  const unsigned int max_brightness_perecent = 100;
+  for(int led_1_brightness_percent = 0;  led_1_brightness_percent <= max_brightness_perecent; led_1_brightness_percent++) {
+    delay(duration_to_breathin_s * 1000 / max_brightness_perecent);
+    analogWrite(led_1_pin, conv_percent_to_ADC(led_1_brightness_percent));
+  }
+}
+
+int conv_percent_to_ADC(int input) {
+  const unsigned int steps_of_ADC = 255;
+  return input * steps_of_ADC / 100;
 }
