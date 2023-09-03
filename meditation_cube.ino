230080e190afa3757f35ce70fb326735d4db3e26
Meng Guo <Meng.guo.HH@gmail.com>
2023-09-03 17:50:29 +0200

    initializing meditation cube repo

---
+++ meditation_cube.ino
@@ -0,0 +1,12 @@
+// Meditation cube firmware 
+// by Meng Guo
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
