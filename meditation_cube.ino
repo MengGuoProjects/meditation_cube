66a0f367c4157cc34bdbbe88677968f857cd262e
Meng Guo <Meng.guo.HH@gmail.com>
2023-09-04 00:33:51 +0200

    feat: implement simple breath out

--- meditation_cube.ino
+++ meditation_cube.ino
@@ -2,29 +2,43 @@
 // by Meng Guo
 
 /* global variables */
-const unsigned int led_1_pin = 11;
+const unsigned int LED_1_PIN = 11;
+const unsigned int MAX_BRIGHTNESS_PERCENT = 100;
 
 /* initializtion */
 void setup() {
-  pinMode(led_1_pin, OUTPUT); 
+  pinMode(LED_1_PIN, OUTPUT); 
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
 /* forward clarations */
-void breath_in(const unsigned int duration_to_breathin_s);
 int conv_percent_to_ADC(int input);
+void breath(const unsigned int duration_to_breathin_s, const bool is_exhale = false);
 
 /* main function */
 void loop() {
-  const unsigned int time_breath_in_s = 5;
-  breath_in(time_breath_in_s);
+  const unsigned int inhale_s = 5;
+  const unsigned int exhale_s = 7;
+  const unsigned int hold_ms = 500;
+  breath(inhale_s);
+  delay(hold_ms * 2);
+  breath(exhale_s, true);
+  delay(hold_ms);
 }
 
 /* helper functions */
-void breath_in(const unsigned int duration_to_breathin_s) {
-  const unsigned int max_brightness_perecent = 100;
-  for(int led_1_brightness_percent = 0;  led_1_brightness_percent <= max_brightness_perecent; led_1_brightness_percent++) {
-    delay(duration_to_breathin_s * 1000 / max_brightness_perecent);
-    analogWrite(led_1_pin, conv_percent_to_ADC(led_1_brightness_percent));
+void breath(const unsigned int duration_to_breathin_s, const bool is_exhale = false) {
+  for(int brightness_ramp_up_percent = 0;  brightness_ramp_up_percent <= MAX_BRIGHTNESS_PERCENT; brightness_ramp_up_percent++) {
+    const unsigned int brightness_ramp_down_percent = 100 - brightness_ramp_up_percent;
+
+    if (is_exhale) {
+      analogWrite(LED_1_PIN, conv_percent_to_ADC(brightness_ramp_down_percent));
+    } else {
+      analogWrite(LED_1_PIN, conv_percent_to_ADC(brightness_ramp_up_percent));
+    }
+
+    delay(duration_to_breathin_s * 1000 / MAX_BRIGHTNESS_PERCENT);
   }
 }
 
