06bb9139e4cbb55081350e69fee016ca70695048
Meng Guo <Meng.guo.HH@gmail.com>
2023-09-16 13:51:16 +0200

    feat: WIP add encoder reading for breathing duration

--- meditation_cube.ino
+++ meditation_cube.ino
@@ -1,34 +1,52 @@
 // Meditation cube firmware 
 // by Meng Guo
 
+#include <Encoder.h>
+
 /* global variables */
 const unsigned int LED_1_PIN = 11;
 const unsigned int MAX_BRIGHTNESS_PERCENT = 100;
+const unsigned int ENC_1_PIN = 1;
+const unsigned int ENC_2_PIN = 2;
+const unsigned int MAX_INHALE_MS = 10000;
+
+unsigned int inhale_ms = 0;
+unsigned long encoder_new_position = 0;
+unsigned long encoder_old_position = 0;
+const unsigned long encoder_step_size = 4;
+
+Encoder myEnc(ENC_1_PIN, ENC_2_PIN);
+
 
 /* initializtion */
 void setup() {
+  pinMode(ENC_1_PIN, INPUT_PULLUP);
+  pinMode(ENC_2_PIN, INPUT_PULLUP);
   pinMode(LED_1_PIN, OUTPUT); 
-  startupAnimation();
+  // startupAnimation();
   digitalWrite(LED_1_PIN, LOW);
+  Serial.begin(9600);
+  Serial.println("Encoder Test:");
 }
 
 /* forward clarations */
 int convPercentToADC(int input);
-void breath(const unsigned int duration_to_breathin_s, const bool is_exhale = false);
+void breath(const unsigned int duration_to_breathin_ms, const bool is_exhale = false);
+unsigned int convEncToMilliSec(long input);
 
 /* main function */
 void loop() {
-  const unsigned int inhale_s = 5;
-  const unsigned int exhale_s = 7;
+  inhale_ms = convEncToMilliSec(myEnc.read());
+  const unsigned int exhale_ms = inhale_ms * 7 / 5;
   const unsigned int hold_ms = 500;
-  breath(inhale_s);
-  delay(hold_ms * 2);
-  breath(exhale_s, true);
-  delay(hold_ms);
+  breath(inhale_ms*10);
+  // delay(hold_ms * 2);
+  // breath(exhale_ms, true);
+  // delay(hold_ms);
 }
 
 /* helper functions */
-void breath(const unsigned int duration_to_breathin_s, const bool is_exhale = false) {
+void breath(const unsigned int duration_to_breathin_ms, const bool is_exhale = false) {
   for(int brightness_ramp_up_percent = 0;  brightness_ramp_up_percent <= MAX_BRIGHTNESS_PERCENT; brightness_ramp_up_percent++) {
     const unsigned int brightness_ramp_down_percent = 100 - brightness_ramp_up_percent;
 
@@ -38,7 +56,7 @@ void breath(const unsigned int duration_to_breathin_s, const bool is_exhale = fa
       analogWrite(LED_1_PIN, convPercentToADC(brightness_ramp_up_percent));
     }
 
-    delay(duration_to_breathin_s * 1000 / MAX_BRIGHTNESS_PERCENT);
+    delay(duration_to_breathin_ms / MAX_BRIGHTNESS_PERCENT);
   }
 }
 
@@ -51,4 +69,18 @@ void startupAnimation() {
   for (int i = 0; i < 4; i++) {
     breath(i);
   }
+}
+
+unsigned int convEncToMilliSec(long encoder_new_position) {
+  unsigned long encoder_position_single_step = encoder_old_position / encoder_step_size;
+    if ((encoder_new_position > encoder_old_position + 1) || (encoder_new_position < encoder_old_position - 1)) {
+      encoder_old_position = encoder_new_position;
+      if ((encoder_new_position / encoder_step_size) > MAX_INHALE_MS) {
+        encoder_new_position = MAX_INHALE_MS * encoder_step_size;
+      }
+      encoder_position_single_step = encoder_new_position / encoder_step_size;
+      Serial.print(encoder_position_single_step);
+      Serial.println();
+    }
+  return static_cast<unsigned int>(encoder_position_single_step);
 }
